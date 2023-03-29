49f969adb4c06b8175fe4dea489daa8e92624ddf
Rania84 <raniamoanes84@gmail.com>
2023-03-29 19:18:07 -0400

    the Basic structure of the car

---
+++ src/BasicStucture.java
@@ -0,0 +1,5 @@
+public class BasicStucture {
+    String model;
+    String power;
+    String color;
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BasicStucture adui = new BasicStucture();
+        audi.color = "black";
+        audi.model = "A3";
+        audi.power = "5000cc";
+    }
+}
