7a8be50f0164f2dda7f8cf8c2d3f0c73521b07a6
Rania84 <raniamoanes84@gmail.com>
2023-03-29 20:15:22 -0400

    the engine specs has been added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        BasicStucture adui = new BasicStucture();
+        BasicStucture audi = new BasicStucture();
         audi.color = "black";
         audi.model = "A3";
         audi.power = "5000cc";
+
+
+        Engine X =new Engine();
+        X.cylinders="8";
+        X.size="1234cc";
+        X.weight="6453k";
+
+
     }
 }
